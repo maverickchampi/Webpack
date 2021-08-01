7fb4d02cdfdfbbe6c200751daf000d69e81ee06a
Maverick <maverick.champi@gmail.com>
2021-08-01 18:44:18 -0500

    git bundle analyzer agregado

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,6 +3,8 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const CopyPlugin = require("copy-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./src/index.js",
@@ -12,6 +14,7 @@ module.exports = {
     assetModuleFilename: "assets/images/[hash][ext][query]",
   },
   mode: "development",
+  devtool: "source-map",
   //   watch: true,
   resolve: {
     extensions: [".js"],
@@ -74,6 +77,7 @@ module.exports = {
     //   ],
     // }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     contentBase: path.join(__dirname, "dist"),
